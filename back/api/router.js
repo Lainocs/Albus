ea2a0211f580c96d74f5c401d3980d02bf268890
Nicolas de Garrigues <ndegarrigues@gmail.com>
2023-02-02 19:26:38 +0100

    :sparkles: FEAT routes folder

--- back/api/router.js
+++ back/api/router.js
@@ -1,8 +1,13 @@
 import express from 'express'
 import UserController from './Controllers/UserController.js'
+import UserRoutes from './Routes/UserRoutes.js'
 
 const router = express.Router()
 
-router.get('/', UserController.getUsers)
+router.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+router.use('/users', UserRoutes)
 
 export default router
